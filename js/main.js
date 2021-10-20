200b8abff2b3e10d1a7082c1a8ad943c8d7a681f
jeevan <jeevanparajuli856@gmail.com>
2021-10-20 10:52:38 +0545

    complete finish except text upto about me

--- js/main.js
+++ js/main.js
@@ -13,3 +13,22 @@ function display() {
 
 
 // End of navigation part
+
+// start animation triger when elemenent is seen 
+
+const startAnimation = (entries, observer) => {
+    entries.forEach(entry => {
+        entry.target.classList.toggle("st", entry.isIntersecting);
+    });
+};
+
+const observer = new IntersectionObserver(startAnimation);
+const options = { root: null, rootMargin: '0px', threshold: 1 };
+
+const elements = document.querySelectorAll('.statistic');
+elements.forEach(el => {
+    observer.observe(el, options);
+});
+
+
+// end animation triger when elemenent is seen
